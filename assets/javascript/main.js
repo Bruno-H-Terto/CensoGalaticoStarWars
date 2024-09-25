1b8b8eeaff65537ba90764475ccebd05893fc2fe
Bruno-H-Terto <herculanoterto@gmail.com>
2024-09-25 03:10:25 -0300

    Conclusão do passo 2

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,27 @@
-const URL ='https://swapi.dev/api/planets/';
+window.onload = createBtnPlanet;
+
+const URL = 'https://swapi.dev/api/planets/';
+const list_planets = document.getElementById('list_planets');
+let list = [];
 
 async function requestAPI() {
-  const resp = await fetch(URL);
-  const response = await resp.json();
-  console.log(response.results);
+  const data = await fetch(URL);
+  const response = await data.json();
+  const planets = response.results;
+  console.log(planets);
+
+  return planets;
 }
 
-requestAPI();
+async function createBtnPlanet() {
+  list = await requestAPI();
+
+  list.forEach(planet => {
+
+    let btn = document.createElement('button');
+    btn.textContent = planet.name;
+
+
+    list_planets.appendChild(btn);
+  });
+}
