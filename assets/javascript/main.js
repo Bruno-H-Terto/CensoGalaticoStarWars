28d7a368aa410d8ecc11ee78615b23ff41e69f13
Bruno-H-Terto <herculanoterto@gmail.com>
2024-09-25 03:30:03 -0300

    Conclusão do passo 3. Botões quando clicados retornam dados do planeta como: Nome, clima, terreno e população

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,6 +2,7 @@ window.onload = createBtnPlanet;
 
 const URL = 'https://swapi.dev/api/planets/';
 const list_planets = document.getElementById('list_planets');
+const details_planet = document.getElementById('details_planet');
 let list = [];
 
 async function requestAPI() {
@@ -15,13 +16,31 @@ async function requestAPI() {
 
 async function createBtnPlanet() {
   list = await requestAPI();
+  let i = 0;
 
   list.forEach(planet => {
 
     let btn = document.createElement('button');
     btn.textContent = planet.name;
+    btn.setAttribute('onclick', `dataPlanet(${i})`);
 
 
     list_planets.appendChild(btn);
+    i++;
   });
+}
+
+function dataPlanet(index){
+  details_planet.innerHTML = '';
+
+  let topic_list = document.createElement('ul');
+  topic_list.innerHTML = `
+    <li>${list[index].name}</li>
+    <li>${list[index].climate}</li>
+    <li>${list[index].population}</li>
+    <li>${list[index].terrain}</li>
+  
+  `;
+
+  details_planet.appendChild(topic_list);
 }
